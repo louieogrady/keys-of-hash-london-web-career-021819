2bfe8fa82bda44786126fddcfe798be04d412dba
louieogrady <l.ogrady@gmail.com>
2019-02-17 17:08:58 +0000

    final

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,12 @@
 class Hash
-  def keys_of(arguments)
-    # code goes here
-  end
-end
+  def keys_of(*arguments)
+    select { |key, value| arguments.include? value }.keys
+      end
+    end
+
+# Example of monkey patching written for the hash class.
+# This could be called on a hash.
+# .keys_of
+# This will take in a number of arguments pass them to the given hash
+# and check whether the arguments given are included in the values
+# of the hash and return the keys related to those values if true
